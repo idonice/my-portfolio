031e0072eba51255f975b073c043b17e18c22abc
idonice <idonice3@gmail.com>
2024-07-26 02:48:11 +0300

    style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,6 +18,8 @@ const Header: React.FC = () => {
                     <li><a href="#about" onClick={(e) => scrollToSection(e, 'about')}>About</a></li>
                     <li><a href="#projects" onClick={(e) => scrollToSection(e, 'projects')}>Projects</a></li>
                     <li><a href="#contact" onClick={(e) => scrollToSection(e, 'contact')}>Contact</a></li>
+                    <li className='cv'><a href="/cv.pdf" download="Ido_Nice_CV.pdf">Download CV</a></li>
+
                 </ul>
             </nav>
         </header>
