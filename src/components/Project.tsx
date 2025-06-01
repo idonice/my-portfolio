5def783d1d4d8899c8d0b6fe136932301de33598
idonice <idonice3@gmail.com>
2025-06-01 12:02:05 +0300

    preview popup, logo,

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,25 +1,72 @@
-import React, { useState } from 'react';
-import { PacmanLoader } from 'react-spinners';
-
-import '../style/Projects.css';
+import React, { ReactNode, useState } from "react";
+import "../style/Projects.css";
 
 interface ProjectProps {
-    name: string;
-    link?: string;
-    description: string
-    children: ReactNode;
+  name: string;
+  link?: string;
+  description: string;
+  comingSoon?: boolean;
+  previewVideoUrl?: string;
+  children: ReactNode;
 }
 
-const Project: React.FC<ProjectProps> = ({ name, description, link, children }) => {
-    return (
-        <a href={link} target="_blank" rel="noopener noreferrer" className="project-link">
-            <div className='project'>
-                {children}
-                <span className='project-name'>{name}</span>
-                <span className='project-description'>{description}</span>
-            </div>
-        </a>
-    );
+const Project: React.FC<ProjectProps> = ({
+  name,
+  description,
+  link,
+  comingSoon,
+  previewVideoUrl,
+  children,
+}) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return (
+    <>
+      <a
+        href={link}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="project-link"
+      >
+        <div className="project">
+          {children}
+          <span className="project-name">{name}</span>
+          <span className="project-description">{description}</span>
+          {comingSoon && (
+            <span className="project-description">
+              coming soon |{" "}
+              <a
+                onClick={(e) => {
+                  e.preventDefault();
+                  setIsModalOpen(true);
+                }}
+                style={{ textDecoration: "underline", cursor: "pointer" }}
+              >
+                click here for preview
+              </a>
+            </span>
+          )}
+        </div>
+      </a>
+
+      {isModalOpen && previewVideoUrl && (
+        <div className="modal-overlay" onClick={() => setIsModalOpen(false)}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <button
+              className="close-button"
+              onClick={() => setIsModalOpen(false)}
+            >
+              &times;
+            </button>
+            <video width="100%" height="auto" controls autoPlay>
+              <source src={previewVideoUrl} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Project;
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,22 +1,44 @@
-import React from 'react';
-import '../style/Projects.css';
-import { PacmanLoader } from 'react-spinners';
-import { BsEggFried } from "react-icons/bs";
+import React from "react";
+import logo from "../assets/images/logo.png";
+
+import "../style/Projects.css";
 import { GiTicTacToe } from "react-icons/gi";
 
-import Project from './Project';
+import Project from "./Project";
 
 const Projects: React.FC = () => {
-    return (
-        <section id="projects" className="section projects">
-            <h2>Projects</h2>
-            <div className="project-list">
-                <Project name='Real-time Tic-Tac-Toe' description='React | Node  | Socket.io' link='https://web-production-2d67.up.railway.app/'>
-                    <GiTicTacToe size={80} color='black' />
-                </Project>
-            </div>
-        </section>
-    );
+  return (
+    <section id="projects" className="section projects">
+      <h2>Projects</h2>
+      <div className="project-list">
+        <Project
+          name="Real-time Tic-Tac-Toe"
+          description="React | Node | Socket.io"
+          link="https://tic-tac-toe-acvm.onrender.com/"
+        >
+          <GiTicTacToe size={80} color="black" />
+        </Project>
+        <Project
+          name="YOOP - Connect player together"
+          description="Next.js"
+          comingSoon={true}
+        >
+          <div
+            style={{
+              backgroundColor: "#0088ed",
+              height: "80px",
+              width: "80px",
+              backgroundImage: `url(${logo})`,
+              backgroundSize: "80%",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+              borderRadius: "50%",
+            }}
+          />
+        </Project>
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
